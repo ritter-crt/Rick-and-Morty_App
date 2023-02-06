d40e339ee36ccf1e2c4bf7a85a3e2365949765b6
YOUR NAME <ritter.christina.r.c@gmail.com>
2023-02-06 16:19:50 +0100

    refactored components

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 import { createCharacterCard } from "./components/card/card.js";
+import { createPrevButton } from "./components/nav-pagination/nav-pagination.js";
+import { createNextButton } from "./components/nav-pagination/nav-pagination.js";
+import { createPagination } from "./components/nav-pagination/nav-pagination.js";
+
+createPrevButton()
+createPagination()
+createNextButton()
 
 const cardContainer = document.querySelector('[data-js="card-container"]');
 const searchBarContainer = document.querySelector(
   '[data-js="search-bar-container"]'
 );
 const searchBar = document.querySelector('[data-js="search-bar"]');
-const navigation = document.querySelector('[data-js="navigation"]');
+// const navigation = document.querySelector('[data-js="navigation"]');
 const prevButton = document.querySelector('[data-js="button-prev"]');
 const nextButton = document.querySelector('[data-js="button-next"]');
 const pagination = document.querySelector('[data-js="pagination"]');
+
 
 // States
 let maxPage = 1;
@@ -30,7 +38,7 @@ async function fetchCharacters(searchQuery, pageNumber) {
       }
     })
     maxPage = data.info.pages
-    pagination.textContent = `${page}/${maxPage}`
+    // pagination.textContent = `${page}/${maxPage}`
     refactoredData.forEach((character) => {
       const characterCard = createCharacterCard(character);
       cardContainer.append(characterCard);
@@ -58,6 +66,7 @@ prevButton.addEventListener('click', () => {
   page--
   fetchCharacters('', page)
 })
+
 nextButton.addEventListener('click', () => {
   cardContainer.innerHTML = ''
   page++
