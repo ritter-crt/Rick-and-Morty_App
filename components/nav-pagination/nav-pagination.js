d21f95f2136bf31fc3af9edd6cccbefc7d704745
Yorick <yorick.tenfeld@gmail.com>
2023-02-06 17:03:13 +0100

    changed pagination text

--- components/nav-pagination/nav-pagination.js
+++ components/nav-pagination/nav-pagination.js
@@ -4,27 +4,24 @@
 const navigation = document.querySelector('[data-js="navigation"]');
 
 export function createPrevButton() {
-    const prevButton = document.createElement("button");
-    prevButton.classList.add("button", "button--prev");
-    prevButton.setAttribute("data-js", "button-prev")
-    prevButton.textContent= "previous"
-    navigation.append(prevButton)
+  const prevButton = document.createElement("button");
+  prevButton.classList.add("button", "button--prev");
+  prevButton.setAttribute("data-js", "button-prev")
+  prevButton.textContent = "previous"
+  navigation.append(prevButton)
 }
 
-export function createPagination(){
-    const pagination = document.createElement("span")
-    pagination.classList.add("navigation__pagination")
-    pagination.setAttribute("data-js", "pagination")
-    pagination.textContent = "1/1"
-    navigation.append(pagination)
+export function createPagination() {
+  const pagination = document.createElement("span")
+  pagination.classList.add("navigation__pagination")
+  pagination.setAttribute("data-js", "pagination")
+  navigation.append(pagination)
 }
 
-    export function createNextButton() {
-    const nextButton = document.createElement("button");
-    nextButton.classList.add("button", "button--next");
-    nextButton.setAttribute("data-js", "button-next")
-    nextButton.textContent= "next"
-    navigation.append(nextButton)
+export function createNextButton() {
+  const nextButton = document.createElement("button");
+  nextButton.classList.add("button", "button--next");
+  nextButton.setAttribute("data-js", "button-next")
+  nextButton.textContent = "next"
+  navigation.append(nextButton)
 }
-
-
